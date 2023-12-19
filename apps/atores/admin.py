e46bd0e0e7e3006e9f410dfad3440e60a1d94ac8
Thércio Rodrigues <thercior@gmail.com>
2023-12-19 16:41:41 -0300

    Finalizando app 'atores' e registrando no admin

--- apps/atores/admin.py
+++ apps/atores/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from atores.models import Ator
 # Register your models here.
+
+@admin.register(Ator)
+class AtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'nascimento', 'nacionalidade')
