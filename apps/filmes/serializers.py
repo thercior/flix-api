c4cdf439d05a87dd6138779b903a3412b5f32902
Thércio Rodrigues <thercior@gmail.com>
2023-12-23 12:03:48 -0300

    Criação validação de dados em serializers: validação do campo resumo (limite de caracteres) do app filmes

--- apps/filmes/serializers.py
+++ apps/filmes/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from filmes.models import Filme
 
@@ -6,3 +7,15 @@ class FilmeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Filme
         fields = '__all__'
+    
+    # def get_rate(self, obj):
+    #     average_rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+        
+    #     if average_rate:
+    #         return round(average_rate, 1)
+    
+    def validate_resumo(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError('O campo resumo não pode ser maior que  200 caracteres')
+        return value
+    
