3eb191a9dd9091b8c1a316747e15e6debc56d0be
Thércio Rodrigues <thercior@gmail.com>
2024-01-05 12:34:13 -0300

    correção da view da rota de authentication

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -4,5 +4,5 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 app_name = 'Authentication'
 
 urlpatterns = [
-    path('authencation/token/', TokenObtainPairView, name='token_obtain_pair'),
+    path('authencation/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
