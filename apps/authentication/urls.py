3fc275c0111adf457d0ef7bf0838a47b3f9fb8cc
Thércio Rodrigues <thercior@gmail.com>
2024-01-05 18:57:32 -0300

    Adição das rotas verify e refresh do token do app authentication

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 app_name = 'Authentication'
 
 urlpatterns = [
     path('authentication/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('authentication/token/refres/',TokenRefreshView.as_view(), name='token_refresh'),
+    path('authentication/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
