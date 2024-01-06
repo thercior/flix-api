b33f5482e87bbf331d47334104900a6e5941ce19
Thércio Rodrigues <thercior@gmail.com>
2024-01-06 13:59:18 -0300

    Implantação da logíca de permissão para acesso as views de acordo com método de requisição

--- apps/generos/permissions.py
+++ apps/generos/permissions.py
@@ -3,5 +3,10 @@ from rest_framework import permissions
 class GeneroPermissions(permissions.BasePermission):
     
     def has_permission(self, request, view):
+        if request.method in ['GET', 'OPTIONS', 'HEAD']:
+            return request.user.has_perm('generos.view_genero')
         
-        return True
+        if request.method == 'POST':
+            return request.user.has_perm('generos.add_genero')
+        
+        return False
