556c4a54274c51f42f791ad62b6b7049b70328b2
Thércio Rodrigues <thercior@gmail.com>
2024-01-21 11:31:50 -0300

    Add config: configurações de media e alteração posição config statics

--- config/settings.py
+++ config/settings.py
@@ -148,9 +148,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Media Files (CSS, JavaScript, Images)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
