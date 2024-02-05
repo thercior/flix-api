5b18137d6e4d952cfd324ca1216bcb4c383f93d1
Thércio Rodrigues <thercior@gmail.com>
2024-02-05 11:01:16 -0300

    Inclusão de autenticação por login ao rest_framework e instalação da lib django-filters para adicionar campos de filtragem ao DRF

--- config/settings.py
+++ config/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'apps.reviews',
     'rest_framework',
     'rest_framework_simplejwt',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -156,9 +157,14 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DATA_INPUT_FORMATS': ['%d/%m/%Y', '%Y-%m-%d', '%d-%m-%y', '%d-%m-%Y']
 }
 
 SIMPLE_JWT = {
