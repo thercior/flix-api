a2b7e700909b64a24c211fea9e3c41f6536df030
Thércio Rodrigues <thercior@gmail.com>
2024-02-05 12:12:45 -0300

    Adição da serialização de atores e gênero para exibir os nomes no endpoint de reviews

--- apps/reviews/serializers.py
+++ apps/reviews/serializers.py
@@ -1,16 +1,20 @@
 from rest_framework import serializers
+from atores.serializers import AtorSerializer
 from filmes.serializers import FilmeSerializer
+from generos.serializers import GeneroSerializer
 from reviews.models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Review
         fields = '__all__'
 
 class ReviewListDetailSerializer(serializers.ModelSerializer):
     filme = FilmeSerializer()
-    
+    atores = AtorSerializer(many=True)
+    genero = GeneroSerializer()
+
     class Meta:
         model = Review
         fields = ['id', 'filme', 'stars', 'comentario']
