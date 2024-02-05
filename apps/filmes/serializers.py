cf74ceb378a9243a723fc179c657143865b1e4d8
Thércio Rodrigues <thercior@gmail.com>
2024-02-05 10:28:19 -0300

    Serializer modificado e verificação de requisição para exibir nome de Gêneros e de Atores na endpoint de Filmes

--- apps/filmes/serializers.py
+++ apps/filmes/serializers.py
@@ -1,24 +1,33 @@
 from django.db.models import Avg
 from rest_framework import serializers
+from atores.serializers import AtorSerializer
+from generos.serializers import GeneroSerializer
 from filmes.models import Filme
 
 class FilmeSerializer(serializers.ModelSerializer):
-    avaliacao = serializers.SerializerMethodField(read_only=True)
-    
+
     class Meta:
         model = Filme
         fields = '__all__'
-    
-    def get_avaliacao(self, obj):
-        average_avaliacao = obj.reviews.aggregate(Avg('stars'))['stars__avg']
-        
-        if average_avaliacao:
-            return round(average_avaliacao, 1)
-    
+
     def validate_resumo(self, value):
         if len(value) > 1000:
             raise serializers.ValidationError('O campo resumo não pode ser maior que 1000 caracteres')
         return value
+
+class FilmeListDetailSerializer(serializers.ModelSerializer):
+    atores = AtorSerializer(many=True) # vários atores - uma lista de atores, por isso Many = True
+    genero = GeneroSerializer() # só vem um dado, logo o padrão Many = False
+    avaliacao = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = Filme
+        fields = ['id', 'titulo', 'genero', 'atores', 'ano', 'avaliacao', 'resumo']
+
+    def get_avaliacao(self, obj):
+        average_avaliacao = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if average_avaliacao:
+            return round(average_avaliacao, 1)
 
 class FilmeStatisticsSerializer(serializers.Serializer):
     total_filmes = serializers.IntegerField()
