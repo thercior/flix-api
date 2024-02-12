86cbae56a45606577fb041ab475ebb27b613f923
Thércio Rodrigues <thercior@gmail.com>
2024-02-12 19:20:17 -0300

    Correção do __str__

--- apps/atores/models.py
+++ apps/atores/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 NACIONALIDADE_CHOICES = (
     ('EUA', 'Estados Unidos'),
@@ -14,10 +13,11 @@ NACIONALIDADE_CHOICES = (
     ('FRA', 'França'),
 )
 
+
 class Ator(models.Model):
     nome = models.CharField(max_length=200)
     nascimento = models.DateField(null=True, blank=True)
     nacionalidade = models.CharField(max_length=100, choices=NACIONALIDADE_CHOICES, blank=True, null=True)
-    
+
     def __str__(self):
-        return self.name
+        return self.nome
