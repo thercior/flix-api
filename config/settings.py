04860fa4070411bc6b935b723010e08a95fcb815
Thércio Rodrigues <thercior@gmail.com>
2024-03-23 11:17:15 -0300

    :wrench: chore: corrigndo tempo de autenticação do token JWT

--- config/settings.py
+++ config/settings.py
@@ -174,6 +174,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
