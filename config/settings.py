f5edfa4df500228900ca7810aaa1336663a2f847
Thércio Rodrigues <thercior@gmail.com>
2024-03-25 11:43:50 -0300

    :wrench: chore: Alteração config do REST_FRAMEWORK: tirando a paginação

--- config/settings.py
+++ config/settings.py
@@ -163,8 +163,8 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
