8bfcf85236e64fae2b88ba2080bbbe79f59e119b
Thércio Rodrigues <thercior@gmail.com>
2024-08-26 20:54:59 -0300

    :sparkles: feat: implementado método para exibir e enviar o nome do país ao invés da sigla

--- apps/atores/serializers.py
+++ apps/atores/serializers.py
@@ -3,7 +3,11 @@ from atores.models import Ator
 
 
 class AtorSerializer(serializers.ModelSerializer):
+    nacionalidade = serializers.SerializerMethodField
 
     class Meta:
         model = Ator
         fields = '__all__'
+
+    def get_nacionalidade(self, obj):
+        return obj.get_nacionalidade_display()
