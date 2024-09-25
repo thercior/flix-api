30d0419e8576f2486381332673d97106c3aa1ffe
Thércio Rodrigues <thercior@gmail.com>
2024-09-25 17:14:37 -0300

    :sparkles: feat: atualização dos nomes exibição das rotas da API Root

--- config/views.py
+++ config/views.py
@@ -9,16 +9,16 @@ class APIRootView(APIView):
 
     def get(self, request, *args, **kwargs):
         return Response({
-            'atores': reverse('Atores:ator_create_list', request=request),
-            'atores-detalhe': reverse('Atores:ator_detalhes', kwargs={'pk': 1}, request=request),
-            'filmes': reverse('Filmes:filme_create_list', request=request),
-            'filmes-detalhe': reverse('Filmes:filme_detail_list', kwargs={'pk': 1}, request=request),
-            'filmes-estatísticas': reverse('Filmes:filme_statistics', request=request),
-            'gêneros': reverse('Generos:genero_create_list', request=request),
-            'gêneros-detalhe': reverse('Generos:genero_detalhes', kwargs={'pk': 1}, request=request),
-            'reviews': reverse('Reviews:reviews_create_list', request=request),
-            'reviews-detalhe': reverse('Reviews:reviews_detalhes', kwargs={'pk': 1}, request=request),
-            'obter_token': reverse('Authentication:token_obtain_pair', request=request),
-            'refresh_token': reverse('Authentication:token_refresh', request=request),
-            'verify_token': reverse('Authentication:token_verify', request=request),
+            'Criação e Lista de Atores': reverse('Atores:ator_create_list', request=request),
+            'Detalhes, Atualização e Exclusão de Atores': reverse('Atores:ator_detalhes', kwargs={'pk': 1}, request=request),
+            'Criação e Lista de filmes': reverse('Filmes:filme_create_list', request=request),
+            'Detalhes, Atualização e Exclusão de filmes': reverse('Filmes:filme_detail_list', kwargs={'pk': 1}, request=request),
+            'Estatísticas de filmes': reverse('Filmes:filme_statistics', request=request),
+            'Criação e Lista de gêneros': reverse('Generos:genero_create_list', request=request),
+            'Detalhes, Atualização e Exclusão de gêneros': reverse('Generos:genero_detalhes', kwargs={'pk': 1}, request=request),
+            'Criação e Lista de reviews': reverse('Reviews:reviews_create_list', request=request),
+            'Detalhes, Atualização e Exclusão de reviews': reverse('Reviews:reviews_detalhes', kwargs={'pk': 1}, request=request),
+            'Obtenção de token de validação': reverse('Authentication:token_obtain_pair', request=request),
+            'Atualização de token de validação': reverse('Authentication:token_refresh', request=request),
+            'Verificação de token de validação': reverse('Authentication:token_verify', request=request),
         })
