84cd0a64507786e559494bcbf96df62461b55b02
Thércio Rodrigues <thercior@gmail.com>
2025-01-03 17:20:46 -0300

    :wrench: chore: ajuste de configuração dos staics nas rotas

--- config/urls.py
+++ config/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
